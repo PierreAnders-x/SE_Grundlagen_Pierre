2994c62b260ae7368cbfc5a2f5c5d2e0f16400f6
Michael Lutz <michel.lutz92@gmail.com>
2026-04-16 12:49:41 +0200

    Projektdateien hinzufügen.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿// Ein Kommentar in C# beginnt mit zwei Schrägstrichen (//) und erstreckt sich bis zum Ende der Zeile. Kommentare werden vom Compiler ignoriert und dienen dazu, den Code zu dokumentieren oder zu erklären. In diesem Fall könnte der Kommentar beispielsweise erklären, dass die folgende Zeile den Text "Hello, World!" auf der Konsole ausgibt.
+
+Console.WriteLine("Hello, World!");
+// In unserem Programm wird der Namespace "System" verwendet, um Zugriff auf grundlegende Funktionen wie die Konsolenausgabe zu erhalten. Die Methode "WriteLine" der Klasse "Console" wird aufgerufen, um den Text "Hello, World!" auf der Konsole anzuzeigen.
+
+
+Console.WriteLine("Hallo Welt"); // Ausgabe auf Deutsch
+//Wir schreiben in Anführungszeichen damit der Text als String erkannt wird. Das
+
+//Semikolon am Ende der Zeile signalisiert das Ende der Anweisung. In diesem Fall wird "Hallo Welt" auf der Konsole ausgegeben, was die deutsche Version von "Hello, World!" ist.
+
+
